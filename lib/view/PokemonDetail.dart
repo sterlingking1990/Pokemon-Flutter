ca7ec42cc0aae00116c75f342b90de27cfae6d02
sterlingking1990 <izundukingsleyemeka@gmail.com>
2021-04-16 17:09:22 +0100

    change color for abilities gradient

--- lib/view/PokemonDetail.dart
+++ lib/view/PokemonDetail.dart
@@ -159,7 +159,7 @@ class PokemonDetailState extends State<PokemonDetail> {
                               colors: [
                             Color(0xa9a9333a),
                             Color(0xC3C39999),
-                            Color(0x1111bbbb)
+                            Color(0xffffbf00)
                           ],
                               stops: [
                             0.3,
